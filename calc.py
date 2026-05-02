50e36ffcb50c8e7bc67e5b053441517a029ffe6d
DN <duan_n@outlook.com>
2026-05-02 15:20:26 +0800

    计算土压力系数

--- calc.py
+++ calc.py
@@ -1,3 +1,36 @@
+import csv
+import math
+from typing import List, Dict, Optional
+
+class SoilLayer:
+    """土层类"""
+    def __init__(self, name, thickness, unit_weight, cohesion, friction_angle):
+        self.name = name 
+        self.thickness = thickness 
+        self.unit_weight = unit_weight
+        self.cohesion = cohesion      
+        self.friction_angle = friction_angle
+    
+    def active_coefficient(self):
+        """主动土压力系数 Ka"""
+        phi_rad = math.radians(self.friction_angle)
+        ka = (math.tan(math.pi/4 - phi_rad/2)) ** 2
+        return ka
+
+    def active_cohesion_term(self):
+        phi_rad = math.radians(self.friction_angle)
+        ka_2 = 2 * layer.cohesion * math.sqrt((math.tan(math.pi/4 - phi_rad/2)) ** 2)
+        return ka_2
+    
+    def passive_coefficient(self):
+        """被动土压力系数 Kp"""
+        phi_rad = math.radians(self.friction_angle)
+        kp = (math.tan(math.pi/4 + phi_rad/2)) ** 2
+        return kp
+
+    def passive_cohesion_term(self):
+        kp_2 = 2 * layer.cohesion * round(math.sqrt(self.passive_coefficient()), 3)
+        return kp_2
 
 
 if __name__ == "__main__":
